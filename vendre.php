67fe0a36fafcac851d7a968d5956d682c8674498
Manu FERRARA <titig9122@gmail.com>
2020-04-13 17:49:57 +0200

    passage en php

--- vendre.php
+++ vendre.php
@@ -1,73 +1,78 @@
   
  <!DOCTYPE html> 
+<html> 
 
-<?php 
-$Prenom = "";
-$Adresse1 = "";
-$Adresse2 = "";
-$Ville = "";
-$Cpostal= "";
-$Pays= "";
-$Ntel= "";
-?>
-
-
-<html>
-
- <head> 
+    <head> 
   <title>Ebay ECE</title>
-  <link href="logo.png" rel="icon" type="images/x-icon" /> 
+  <link href="favicon.jpg" rel="icon" type="images/x-icon" /> 
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- </head> 
-
-<body>
+</head> 
+ 
+<body> 
+   
+  
     
     <h1> Ebay ECE </h1>
-    
-    
-   <div id="menu">  
-            <a href="Cat%C3%A9gories.html"><img src="primehomenav.gif" alt="categories" width="107" height="25"/></a>   
-            <a href="Achat.html"> <img src="primehomenav.gif" alt="achat" width="107" height="25"/> </a>   
-            <a href="Vendre.html"><img src="primefinancingnav.gif" alt="vendre" width="107" height="25"/></a>     
-            <a href="Compte.html">  <img src="primecontactnav.gif" alt="compte" width="107" height="25"/></a>
-            <a href="Panier.html"><img src="primefinancingnav.gif" alt="panier" width="107" height="25"/></a>     
-            <a href="Admin.html">  <img src="primecontactnav.gif" alt="admin" width="107" height="25"/></a>
+    <div id="menu">  
+            <a href="Cat%C3%A9gories.php"><img src="primehomenav.gif" alt="categories" width="107" height="25"/></a>   
+            <a href="Achat.php"><img src="primelistingsbtn.gif" alt="achat" width="107" height="25"/> </a>   
+            <a href="Vendre.php"><img src="primefinancingnav.gif" alt="vendre" width="107" height="25"/></a>     
+            <a href="Compte.php">  <img src="primecontactnav.gif" alt="compte" width="107" height="25"/></a>
+            <a href="Panier.php"><img src="primefinancingnav.gif" alt="panier" width="107" height="25"/></a>     
+            <a href="Admin.php">  <img src="primecontactnav.gif" alt="admin" width="107" height="25"/></a>
         </div> 
-    
-    <h2> Mon compte </h2>
-    <h3>Mes informations personnelles</h3>
-<p> Prénom: <?php echo "$Prenom"; ?> </p> 
-<p> Adresse ligne 1: <?php echo "$Adresse1"; ?> </p> 
-<p> Adresse ligne 2: <?php echo "$Adresse2"; ?> </p>
-<p> Ville: <?php echo "$Ville"; ?> </p>
-<p> Code Postal: <?php echo "$Cpostal"; ?> </p>
-<p> Pays: <?php echo "$Pays"; ?> </p>
-<p> N° de léléphone: <?php echo "$Ntel"; ?> </p> 
+    <h2> Vendre </h2>
+ 
+          
+        
+ 
+   <form action="traitementventes.php" method="post">
+       
+ type de vente :<br />
+<input type="radio" name="Tvente" value="ench" /> Enchères
+<input type="radio" name="Tvente" value="offre" /> Meilleure offre
+<input type="radio" name="Tvente" value="achatM" /> Achetez-le maintenant
+<br />
+<p>
+   Ajouter un titre :<br />
+   <input type="text" name="titre" value="" />
+</p>
+   <br />
 
-<h3>Mon historique d'achat</h3>
-<table class="table">  
-    <thead>     
-     <tr>   
-      <th>Photo</th>   
-      <th>Nom</th>   
-      <th>Prix</th> 
-      <th>Info</th> 
-     </tr> 
-    </thead>   
-    <tbody> 
-     <tr>    
-      <td> <img src="ForT.jpeg" style="width: 50%;"> </td>    
-      <td>ferraille ou trésor </td>   
-      <td>250 euros</td> 
-      <td> </td>
-     </tr>   
-    </tbody>  
-   </table> 
+<p>
+   Ajouter une description :<br />
+   <textarea name="description" rows="6" cols="40">
+   </textarea>
+</p>
+  
+    <br />    
+<p>
+   Ajouter un prix :<br />
+   <input type="number" name="prix" value="" />
+</p>
+<br />
+       
+ <p>
+   Date de fin d'enchère:<br />
+   <input type="date" name="finD" value="" />
+   <input type="time" name="finH" value="" />
+</p>
+<br />
+       
+       
+       
+       
+<td colspan="2" align="center">    <input type="submit" value="Valider" />    </td>  
 
-
-</body>
-
+</form>
+ 
+   
+        <div id="footer">Copyright &copy; 2020 Ebay ECE
+            <br>    
+            <a href="mailto:Ebay.ECE@gmail.com">Ebay.ECE@gmail.com</a>  
+        </div>   
+ </body>
 </html>
